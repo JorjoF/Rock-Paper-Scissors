1c023ad15849be83650bb219e5cf7c7046c8f91b
Jorge Fuerte <fuerte1206@gmail.com>
2022-03-26 20:36:03 -0400

    Added win conditions

--- scripts/index.js
+++ scripts/index.js
@@ -42,17 +42,23 @@ function playRound(player, computer, outcomes){
         var v = outcomes[play];
         switch(v){
             case 1: case 2: case 3:
-                return "Tie";
+                return "Tie"
             case 4: case 5: case 6:
                 computerScore++;
                 document.querySelector("#computerScore").innerHTML = computerScore;
-                console.log(computerScore)
-                return "Computer Wins!!!!";
+                if(computerScore === 5){
+                    reset();
+                    return "Computer as won the Game";
+                }
+                return 'Computer wins this round';
             case 7: case 8: case 9:
                 playerScore++;
                 document.querySelector("#playerScore").innerHTML = playerScore;
-                console.log(playerScore)
-                return "Player Wins!!!!";
+                if(playerScore === 5){
+                    reset();
+                    return "Player as won the Game";
+                }
+                return 'Player wins this round';
                 
         }
     }
@@ -72,28 +78,34 @@ function computerToEmoji(computer){
     }
 }
 
+function reset(){
+    computerScore = 0;
+    document.querySelector("#computerScore").innerHTML = 0;
+    playerScore = 0;
+    document.querySelector("#playerScore").innerHTML = 0;
+}
+
 rButton.addEventListener('click', () => {
     const computer = computerPlay()
-    document.getElementById('outcome').innerHTML = playRound("r", computer, gameOutcomes);
+    document.querySelector('#outcome').innerHTML = playRound("r", computer, gameOutcomes);
     document.getElementById('player').innerHTML = rock
     document.getElementById('computer').innerHTML = computerToEmoji(computer)
     roundWinner.showModal();
-
 })
 pButton.addEventListener('click', () => {
     const computer = computerPlay()
-    document.getElementById('outcome').innerHTML = playRound("p", computer, gameOutcomes);
+    document.querySelector('#outcome').innerHTML = playRound("p", computer, gameOutcomes);
     document.getElementById('player').innerHTML = paper
     document.getElementById('computer').innerHTML = computerToEmoji(computer)
     roundWinner.showModal();
+
 })
 sButton.addEventListener('click', () => {
     const computer = computerPlay()
-    document.getElementById('outcome').innerHTML = playRound("s", computer, gameOutcomes);
+    document.querySelector('#outcome').innerHTML = playRound("s", computer, gameOutcomes);
     document.getElementById('player').innerHTML = scissors
     document.getElementById('computer').innerHTML = computerToEmoji(computer)
     roundWinner.showModal();
-
 })
 closeModal.addEventListener('click', () => {
     roundWinner.close();
